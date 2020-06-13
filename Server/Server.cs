ec800db34be54fc49592aa397ea4d5356d6d48fa
ThaiVietThanh <1710266@dlu.edu.vn>
2020-06-13 10:45:42 +0700

    Update Server.cs

--- Server/Server.cs
+++ Server/Server.cs
@@ -59,7 +59,7 @@ namespace Server
         void MoKetNoi()
         {
             clientlist = new List<Socket>();
-            IP = new IPEndPoint(IPAddress.Parse("52.187.22.95"), 9999);
+            IP = new IPEndPoint(IPAddress.Any, 9999);
             server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             server.Bind(IP);
             Thread listen = new Thread(() =>
