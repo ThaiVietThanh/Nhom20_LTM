bd66470703c675728c1acd2385037ed8a37d329d
ThaiVietThanh <1710266@dlu.edu.vn>
2020-06-23 10:09:49 +0700

    Thêm SQLQuery

---
+++ StoredProcedure.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE DangNhap
+@TenDN as varchar(20),
+@MatKhau as varchar(20)
+AS
+BEGIN
+SELECT TenDN, MatKhau FROM users WHERE TenDN=@TenDN AND MatKhau=@MatKhau
+END
---
+++ Table.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[users] (
+    [TenDN]   NCHAR (20) NOT NULL,
+    [MatKhau] NCHAR (20) NULL,
+    CONSTRAINT [PK_users] PRIMARY KEY CLUSTERED ([TenDN] ASC)
+);
